749e4b4d1d1a565ceb9f09b73f0de6e46f4bc4cf
nikakaladze <nikakaladze89@gmail.com>
2024-09-23 20:00:25 +0400

    finish Assignments

---
+++ src/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Header() {
+
+    return (
+        <nav>
+            <div className="main-container">
+
+
+
+
+
+                <div className="end">
+                    <span>Home</span>
+                    <span>Learn</span>
+                    <span>Reference</span>
+                    <span>blog</span>
+                    <span>Contact</span>
+                </div>
+            </div>
+        </nav>
+    )
+}
