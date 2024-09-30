d2d1a8be3f849e4310203ce656a17df492ea2cb2
nikakaladze <nikakaladze89@gmail.com>
2024-10-01 01:56:45 +0400

    work on the assignment side

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from './header/Header';
 import Footer from './footer/Footer'
 import './App.css';
 import { ProductCardContainer } from '../src/ProductCardContainer/ProductCardContainer'
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Contact from './Components/Contact';
+import ProductTableAsssignments3 from './components-3/ProductListAssignments';
+
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
 
       </head>
       <body>
+      <BrowserRouter>
 
         <header>
 
@@ -19,12 +24,20 @@ function App() {
 
         <main>
 
-          <ProductCardContainer />
+            <Routes>
+              <Route path='/Contact' element={<Contact />} />
+              <Route path='/' element={<ProductCardContainer />} />
+              <Route path='/Assignments3' element={<ProductTableAsssignments3 />} />
+                
+              
+            </Routes>
+          
         </main>
 
         <footer>
           < Footer />
         </footer>
+          </BrowserRouter>
       </body>
     </html>
   );
